a036811010a19bb84ca4bcc2930eee1029f30a06
Henning Dierks <henning.dierks@mailbox.tu-dresden.de>
2026-07-18 14:07:02 +0200

    Refactor imports and update __all__ in __init__.py
    Updated imports to include specific classes and added them to __all__ for public access.

--- dplot/__init__.py
+++ dplot/__init__.py
@@ -1,5 +1,24 @@
-from .common import *
-from .legend import *
+from .common import HAlign, VAlign, Thickness, LineStyle, Marker, Environment, GridSetup, TickSetup, AxisSetup, LineSetup, Data
+from .legend import LegendPosition, LegendSetup
 from .figure import Figure
 from .latexgenerator import LatexGenerator
 from .typstgenerator import TypstGenerator
+
+__all__ = [
+    "HAlign",
+    "VAlign",
+    "Thickness",
+    "LineStyle",
+    "Marker",
+    "Environment",
+    "GridSetup",
+    "TickSetup",
+    "AxisSetup",
+    "LineSetup",
+    "Data",
+    "LegendPosition",
+    "LegendSetup",
+    "Figure",
+    "LatexGenerator",
+    "TypstGenerator",
+]
